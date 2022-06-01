4cc25265bf971a01be247c4f6c2a2d66f283b1dc
Intesar Alam <ialam5539@bths.edu>
2022-06-01 16:32:52 -0400

    completed add

--- src/main/java/learn/mvcgamelab/controllers/GuessTheNumberController.java
+++ src/main/java/learn/mvcgamelab/controllers/GuessTheNumberController.java
@@ -2,6 +2,8 @@ package learn.mvcgamelab.controllers;
 
 import learn.mvcgamelab.domain.GuessTheNumberService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,10 @@ public class GuessTheNumberController {
     @GetMapping("/api/guessthenumber")
     public void getGame() {
         // TODO what do we want to return?
+    }
+
+    @PutMapping("/api/guessthenumber/{guess}")
+    public String makeAGuess(@PathVariable int guess){
+        return service.makeAGuess(guess);
     }
 }
